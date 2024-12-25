0b2dd602eb05a4efe8849cc7f30de5cb6e129dc7
James <JamesH737@gmail.com>
2024-12-24 22:14:21 -0800

    Commit to add functionality for enemies and sprite

--- enemy.py
+++ enemy.py
@@ -1,3 +1,15 @@
 import pygame
 
-class Enemy
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, health, animation_list, x, y, speed):
+        pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.health = health
+        self.animation_list = animation_list
+        self.frame_index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+
+        #select starting image
+        self.image = self.animation_list[self.action]
+
